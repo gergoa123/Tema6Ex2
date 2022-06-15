e1e370184e292f4fced16cfac41f0ed39168ce0c
Gergoa123 <gergo3124@gmail.com>
2022-06-15 21:46:32 +0300

    Add project files.

---
+++ Tema6Ex2/Autoturism.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6Ex2
+{
+    class Autoturism
+    {
+        string marca;
+        string numarDeInmatriculare;
+        int capacitateCilindrica;
+
+        public Autoturism(string marca, string numarDeInmatriculare , int capacitateCilindrica)
+        {
+            this.marca = marca;
+            this.numarDeInmatriculare = numarDeInmatriculare;
+            this.capacitateCilindrica = capacitateCilindrica;
+        }
+
+        public void Tipareste()
+        {
+            Console.WriteLine(marca + ", " + numarDeInmatriculare + ", " + capacitateCilindrica);
+        }
+
+
+
+    }
+}
---
+++ Tema6Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tema6Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autoturism a1 = new Autoturism("Matiz", "69 LoL", 1);
+            Autoturism a2 = new Autoturism("Pollo", "420 GaS", 4);
+
+            a1.Tipareste();
+            a2.Tipareste();
+
+
+        }
+    }
+}
